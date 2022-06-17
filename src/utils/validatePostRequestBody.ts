e9a5c24c50ecad87ba765a2138753c9bfe59dbd3
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-17 11:13:29 +0300

    feat: implement validate post reuest body function

---
+++ src/utils/validatePostRequestBody.ts
@@ -0,0 +1,13 @@
+import { IUser } from "../types/IUser";
+
+export function validatePostRequestBody(userData: IUser): boolean {
+  return (
+    "username" in userData &&
+    typeof userData.username === "string" &&
+    "age" in userData &&
+    typeof userData.age === "number" &&
+    "hobbies" in userData &&
+    Array.isArray(userData.hobbies) &&
+    (userData.hobbies.length === 0 || typeof userData.hobbies[0] === "string")
+  );
+}
